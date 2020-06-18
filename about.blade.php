b921aef9a564ba47ee3b3273acc7b226c9c7ba80
Ambrose <ekponoambrose@gmail.com>
2020-06-18 21:48:23 +0100

    Create about.blade.php

---
+++ about.blade.php
@@ -0,0 +1,8 @@
+@section('title', 'About Us')
+
+@section('content')
+    <p>This is my body content of the about us page</p>
+    <p>And also this other about us specific content</p>
+    <p>Kadet let me know if you have any issue</p>
+    <a href="https://laravel.com/docs/7.x/blade">You can use this link for reference</a>
+@endsection
